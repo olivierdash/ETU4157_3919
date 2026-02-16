da07074b40d6103f903ade1566f25c94aa0d4433
olivierdash <otahiriniaina@gmail.com>
2026-02-16 15:34:33 +0300

    dons

--- app/models/Dons.php
+++ app/models/Dons.php
@@ -1,7 +1,9 @@
 <?php
     namespace app\models;
 
+    use Flight;
     use Entity;
+    use PDO;
 
     final class Dons extends Entity
     {
@@ -11,10 +13,14 @@
         public function __construct($quantite, $date){
             $this->quantite = $quantite;
             $this->date = $date;
+            $this->db = Flight::db();
         }
 
         public function getAll() {
-            
+            $sql = "SELECT * FROM Dons";
+            $stmt = $this->db->query($sql);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         }
         public function getQuantite(){
             return $this->quantite;
