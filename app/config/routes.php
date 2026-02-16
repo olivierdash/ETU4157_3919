b32cc1a603c8c0a1d6b0d5bc825be8357a8f2b9f
faniry74 <faniryanthony74@gmail.com>
2026-02-16 16:38:11 +0300

    "fait"

--- app/config/routes.php
+++ app/config/routes.php
@@ -5,6 +5,7 @@ use flight\Engine;
 use flight\net\Router;
 use app\controllers\VilleController;
 use app\controllers\RessourceController;
+use app\controllers\DonsController;
 /** 
  * @var Router $router 
  * @var Engine $app
@@ -15,7 +16,7 @@ $router->group('', function(Router $router) use ($app) {
     
     $router->get('/', function() use ($app) {
         $ressource_lib = RessourceController::getAllWithRessourcesLib();
-        $app->render('dashboard/list', ['ressource_lib' => $ressource_lib]);
+        $app->render('dashboard/list', ['ressource_lib' => $ressource_lib, 'dons' => $lstDons]);
     });    
 
 }, [SecurityHeadersMiddleware::class]);
