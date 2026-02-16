a2f4bf7a7448af61c0d1a6c9f4b50483d856a979
faniry74 <faniryanthony74@gmail.com>
2026-02-16 14:41:35 +0300

    "creation du modele fait"

---
+++ app/models/Entity.php
@@ -0,0 +1,16 @@
+<?php
+
+use PDO;
+
+class Entity{
+    private $id;
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId($id){
+        $this->id = $id;
+    }
+}
+
+?>
